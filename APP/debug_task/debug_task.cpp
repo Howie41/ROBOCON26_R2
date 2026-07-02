7b690a050c33f5c031fd92493177de5535254cdc
Howie41 <fish_howie41@qq.com>
2026-07-03 03:22:38 +0800

    feat: debug_r1_cmd

--- APP/debug_task/debug_task.cpp
+++ APP/debug_task/debug_task.cpp
@@ -8,17 +8,26 @@
 #include "cmsis_os2.h"
 #include "logger.hpp"
 #include "com_config.h"
+#include "topics.hpp"
 
 
 osThreadId_t Debug_TaskHandle;
 extern LoggerQueue logger_queue;
 
+volatile uint8_t debug_r1_cmd{0x00};
+TypedTopicPublisher<pub_qr_code_parsed> qr_code_pub{"qr_code_parsed"};
 
 void debugTask(void *argument) {
 
   for (;;) {
     osDelay(50);
     logger_queue.trySend();
+    if (debug_r1_cmd != 0x00) {
+      pub_qr_code_parsed msg{.data = debug_r1_cmd};
+      qr_code_pub.Publish(msg);
+      logger_queue.log("DEBUG r1_cmd %02X\n", debug_r1_cmd);
+      debug_r1_cmd = 0x00;
+    }
     // logger.log(
     //     "%.1f|%.0f|%.1f|%.1f/"
     //     "%.1f|%.0f|%.1f|%.1f/"
